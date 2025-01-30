ed013b5469fee9800a71871127bde571977889fc
Ridwan <ridwannmuhammad159@gmail.com>
2025-01-30 14:27:43 +0700

    chore: update pakcage

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
 // eslint.config.js
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsparser from "@typescript-eslint/parser";
+const tseslint = require("@typescript-eslint/eslint-plugin");
+const tsparser = require("@typescript-eslint/parser");
 
-export default [
+module.exports = [
   {
     files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {

4467b86db84c97aea5fe42a4ab5867873e6af6e7
fajri <fajri42vandi@gmail.com>
2025-02-03 16:29:25 +0700

    chore: add seeder account

--- prisma/seeder/account.ts
+++ prisma/seeder/account.ts
@@ -1,33 +1,50 @@
 import * as xlsx from "xlsx";
 
+interface AccountData {
+    fullName: string;
+    phoneNumber: string;
+    gender: "male" | "female";
+    email: string;
+    password: string;
+    role: "Admin" | "User";
+    isActive: boolean;
+    isVerified: boolean;
+}
+
 export default async function account(prisma: any): Promise<void> {
-    // Membaca data dari file Excel
     const workbook = xlsx.readFile("./prisma/seeder/data.xlsx");
     const sheetName = workbook.SheetNames[0];
     const sheet = workbook.Sheets[sheetName];
 
-    type Role = "Admin" | "User";
-
-    // Konversi sheet ke format JSON
-    const data: Array<{ email: string, password: string, role: string, isActive: boolean, isVerified: boolean }> = xlsx.utils.sheet_to_json(sheet);
+    const data: AccountData[] = xlsx.utils.sheet_to_json(sheet);
 
     for (const item of data) {
-        try {
-            await prisma.account.create({
-                data: {
-                    email: item.email,
-                    password: item.password,
-                    role: item.role as Role,
-                    isActive: item.isActive,
-                    isVerified: item.isVerified
-
-                },
-            });
-            console.log(`✅ Data ${item.email} berhasil ditambahkan.`);
-        } catch (error) {
-            console.error(`❌ Gagal menambahkan ${item.email}: ${(error as Error).message}`);
-        }
+        await createAccount(prisma, item);
     }
 
     console.log(`🎉 Seeder untuk ${sheetName} selesai!\n`);
+}
+
+async function createAccount(prisma: any, item: AccountData): Promise<void> {
+    try {
+        await prisma.account.create({
+            data: {
+                email: item.email,
+                password: item.password,
+                role: item.role,
+                isActive: item.isActive,
+                isVerified: item.isVerified,
+                user: {
+                    create: {
+                        fullName: item.fullName,
+                        phoneNumber: item.phoneNumber,
+                        gender: item.gender
+                    }
+                }
+            },
+        });
+        console.log(`✅ Data ${item.fullName} berhasil ditambahkan.`);
+    } catch (error) {
+        console.error(`❌ Gagal menambahkan ${item.email}: ${(error as Error).message}`);
+    }
 }
